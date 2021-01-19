3f39c16d7dc83c7672c24070de9499bf47bfcfd8
Patrick <Patrick93@hotmail.de>
2021-01-19 15:44:48 +0100

    Diving Into Custom Providers

--- src/colas/colas.module.ts
+++ src/colas/colas.module.ts
@@ -5,6 +5,7 @@ import { ColasController } from './colas.controller';
 import { ColasService } from './colas.service';
 import { Cola } from './entities/cola.entity';
 import { Flavor } from './entities/flavor.entity';
+
 @Module({
     imports: [TypeOrmModule.forFeature([Cola, Flavor, Event])],
     controllers: [ColasController],
